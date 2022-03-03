c2b7583e678917933b0daf9d7d8ee5ad160a9128
Raqeeb Taiwo <44765711+taiworoqeeb@users.noreply.github.com>
2022-03-04 00:18:38 +0100

    update

--- configs/config.js
+++ configs/config.js
@@ -5,16 +5,7 @@ const database = process.env.POSTGRES_DB;
 const username = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
 
-var db = new Sequelize(database, username, password, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-});
+var db = new Sequelize(process.env.DATABASE_URL);
 
 
 
